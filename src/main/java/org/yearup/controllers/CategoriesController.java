05d5bd72620d4e27641f35cacfc519de4284a019
edomg <edomgezahegn54@gmail.com>
2026-06-22 12:26:08 -0500

    Implement category lookup by ID
    Add GET /categories/{id} in CategoriesController and wire it to
    CategoryService.getById(). Return the category when it exists and throw
    a 404 NOT_FOUND response when the ID is missing.
    
    Implement CategoryService.getById() using CategoryRepository.findById().

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -34,11 +36,15 @@ public class CategoriesController
         return categoryService.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        Category category = categoryService.getById(id);
+
+        if (category == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
     }
 
     // the url to return all products in category 1 would look like this
