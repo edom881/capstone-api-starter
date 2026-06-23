0bdc54c5e443b9f3167ee57d7c05d4636e2953a8
edomg <edomgezahegn54@gmail.com>
2026-06-22 23:11:21 -0500

    Add admin endpoint to delete categories
    Added DELETE /categories/{id} in the categories controller so admins candelete a category.
    
    The controller checks if the category exists first and returns 404 when it does not exist.
    
    Updated CategoryService.delete to delete the category by
    id using the repository.

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -76,12 +77,14 @@ public class CategoriesController
         return categoryService.update(id, category);
     }
 
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteCategory(@PathVariable int id)
     {
-        // delete the category by id and return status 204 No Content
-        return null;
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
